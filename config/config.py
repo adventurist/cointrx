ef484fc7604e928206d654408f04d6932fd1cd02
logicp <emmanuel@openconcept.ca>
2018-02-22 23:16:07 -0500

    same as last

--- config/config.py
+++ config/config.py
@@ -12,12 +12,14 @@ def get_urls(trx_env):
         'tx_request': 'https://app.cointrx.com/transaction/request',
         'blockgen_url': 'https://app.cointrx.com/regtest/generate/block',
         'userbalance_url': 'https://app.cointrx.com/regtest/user-balance',
-        'wif_to_private_url': 'http://localhost:3000/key/from-wif'
+        'wif_to_private_url': 'http://localhost:3000/key/from-wif',
+        'key_gen_url': 'https://app.cointrx.com/keys/btc/regtest/generate'
     } if trx_env == 'SNOWFLAKE' else {
         'base_url': 'http://localhost',
         'tx_app': 'http://localhost:3000/transaction',
         'tx_request': 'http://localhost:6969/transaction/request',
         'blockgen_url': 'http://localhost:6969/regtest/generate/block',
         'userbalance_url': 'http://localhost:6969/regtest/user-balance',
-        'wif_to_private_url': 'http://localhost:3000/key/from-wif'
+        'wif_to_private_url': 'http://localhost:3000/key/from-wif',
+        'key_gen_url': 'http://localhost:6969/keys/btc/regtest/generate'
     }
