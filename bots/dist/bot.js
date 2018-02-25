7a864e50a39148a6d8a9653ad5d5b76391403404
logicp <emmanuel@openconcept.ca>
2018-02-25 13:57:38 -0500

    Adding Skeleton for TradeBots application

---
+++ bots/dist/bot.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_jet_1 = require("node-jet");
+class TradeBot {
+    constructor() {
+        this.peer = new node_jet_1.Peer;
+    }
+}
---
+++ bots/src/bot.ts
@@ -0,0 +1,9 @@
+import {Peer} from 'node-jet'
+
+class TradeBot {
+    private peer: Peer;
+    constructor () {
+        this.peer = new Peer
+    }
+}
+
