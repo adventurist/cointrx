a781e394713a486097588be2f96b86b40a720029
Helios <yougotbenoit@gmail.com>
2018-02-25 22:57:49 +0000

    First commit with coinpool subfolder

---
+++ coinpool/main.php
@@ -0,0 +1,13 @@
+<p><h4>It works!</h4></p>
+
+<p>
+     <h3> Hurray!! </h3>
+</p>
+
+
+<p>
+     <h1> Let the coding <u><i>magic</i></u> <u><b>begin</b></u>!!</h1>
+</p>
+<br />
+This is just something to fill in the space for now while the rest is being programmed.<br />
+Stay tuned for updates!!
