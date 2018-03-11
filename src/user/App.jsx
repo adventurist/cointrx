7c0dcc12bdb9bccbc8bf5df8e46021fbca4978f6
logicp <emmanuel@openconcept.ca>
2018-03-11 16:37:37 -0400

    Profile can now update new key labels through react

--- src/user/App.jsx
+++ src/user/App.jsx
@@ -190,8 +190,6 @@ export class UserKeys extends React.Component {
     }
 
     static propTypes = {
-        // className: PropTypes.string,
-        // icon: PropTypes.string.isRequired, // fontawesome
         mini: PropTypes.bool,
         tooltipPosition: PropTypes.oneOf(['bottom-center', 'top-center', 'bottom-right', 'top-right', 'bottom-left', 'top-left']),
         onClick: PropTypes.func
@@ -201,6 +199,119 @@ export class UserKeys extends React.Component {
         mini: false,
         tooltipPosition: 'top-center'
     };
+
+    render() {
+        const {mini, tooltipPosition} = this.props;
+        const tooltipPos = tooltipPosition.split('-');
+        const tooltip = 'Create new Bitcoin Address'
+        const actions = [
+            <FlatButton
+                label="Save"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.saveKey.bind(this)}
+            />,
+            <FlatButton
+                label="Delete"
+                secondary={true}
+                keyboardFocused={true}
+                onClick={this.disableKey.bind(this)}
+            />,
+            <FlatButton
+                label="Okay"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.handleClose}
+            />
+        ];
+        return (
+            <div id="key-container">
+                <ReactDataGrid
+                    columns={this.state._columns}
+                    rowGetter={this.rowGetter}
+                    rowsCount={this.state._rows.length}
+                    rowHeight={48}
+                    minHeight={500}/>
+                <div style={keyBtnStyles} id="fab-container">
+                    <FloatingActionButton id="btc-key-btn"
+                                          mini={mini}
+                                          onClick={this.requestBtcKey}
+                                          onMouseEnter={() => this.setState({btnHovered: true})}
+                                          onMouseLeave={() => this.setState({btnHovered: false})}>
+
+                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                            <path fill="#000000"
+    d="M4.5,5H8V2H10V5H11.5V2H13.5V5C19,5 19,11 16,11.25C20,11 21,19 13.5,19V22H11.5V19H10V22H8V19H4.5L5,17H6A1,1 0 0,0 7,16V8A1,1 0 0,0 6,7H4.5V5M10,7V11C10,11 14.5,11.25 14.5,9C14.5,6.75 10,7 10,7M10,12.5V17C10,17 15.5,17 15.5,14.75C15.5,12.5 10,12.5 10,12.5Z"
+                            />
+                        </svg>
+
+                    </FloatingActionButton>
+                    {tooltip &&
+                    <Tooltip
+                        show={this.state.btnHovered}
+                        label={tooltip}
+                        style={{fontSize: '9pt'}}
+                        horizontalPosition={tooltipPos[1]}
+                        verticalPosition={tooltipPos[0]}/>
+                    }
+                </div>
+                <Dialog
+                    title="Key Editor"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.dialogOpen}
+                    onRequestClose={this.handleClose}
+                >
+                    <TextField
+                        value={this.state.dialogText}
+                        ref='keylabel'
+                        className='keylabel'
+                        floatingLabelText='Key Label'
+                        onChange={this.dialogTextChange}
+                    />
+                    <br />
+                    Set a date to automatically disable this key <br/>
+                    <div className="datepicker">
+                        <DatePicker hintText="Date Picker" />
+                        <FlatButton label="Set" secondary={true} keyboardFocused={true} onClick={this.setKeyFutureDisable}/>
+                    </div>
+                </Dialog>
+
+            </div>
+        )
+    }
+
+    buildBtns (row) {
+        console.dir(row)
+        return ({
+            'id': row.id,
+            'lbl': row.lbl,
+            'adr': row.adr,
+            'bal': row.bal,
+            'btn':
+                <div>
+                    <RaisedButton label="Edit" secondary={true}
+                                  id={row.id} value={row.id}
+                                  onClick={this.openEditKeyDialog.bind(this, row.id)}
+                    />
+                    {this.state.dialogOpen && isObjectEquivalent(this.props.row, this.state.dialogCursor) ?
+                        (<KeyDialog show={this.state.dialogOpen}/>)
+                        : null}
+                </div>
+        })
+    }
+
+    buildRows(key) {
+        console.dir(key)
+        return {
+            'id': key.id,
+            'lbl': key.label,
+            'adr': key.address,
+            'bal': key.balance,
+            'btn': ''
+
+        }
+    }
 
     requestBtcKey = async() => {
         console.log('New BTC Key requested')
@@ -262,34 +373,6 @@ export class UserKeys extends React.Component {
     createRows = (keyData) => keyData.map(x => this.buildRows(x))
     addBtnRows = (rows) => rows.map(x => this.buildBtns(x))
 
-    buildRows(key) {
-        console.dir(key)
-        return {
-            'id': key.id,
-            'lbl': key.label,
-            'adr': key.address,
-            'bal': key.balance,
-            'btn': ''
-
-        }
-    }
-
-    buildBtns (row) {
-        console.dir(row)
-        return ({
-            'id': row.id,
-            'lbl': row.lbl,
-            'adr': row.adr,
-            'bal': row.bal,
-            'btn': <div><RaisedButton label="Edit" secondary={true} id={row.id} value={row.id}
-                                      onClick={this.openEditKeyDialog.bind(this, row.id)}/>
-                {this.state.dialogOpen && isObjectEquivalent(this.props.row, this.state.dialogCursor) ? (
-                        <KeyDialog show={this.state.dialogOpen}/>) : null}
-            </div>
-        })
-    }
-
-
     rowGetter = (i) => {
         return this.state._rows[i];
     };
@@ -303,7 +386,7 @@ export class UserKeys extends React.Component {
     }
 
     getKeyLabel = (id) => {
-        const key = keyData.find(key => key.id = id)
+        const key = keyData.find(key => key.id == id)
         if (isKeyObject(key)) {
             return key.label
         }
@@ -322,105 +405,34 @@ export class UserKeys extends React.Component {
     requestBtcKeyUpdate = async(id, label) => {
         const csrf = getCsrfToken()
         if (csrf && csrf.length > 0) {
-            const keyUpdateResponse = await fetchKeyUpdate(id, label, csrf)
-            // if (newUserData && Array.isArray(newUserData) && newUserData.length > 0) {
-            //     const keys = Object.keys(newUserData[0])
-            //     if (!keys.includes('error')) {
-            //         console.log('Updating user data')
-            //         const _newRows = newUserData[0].keys
-            //         this.updateState(_newRows)
-            //     } else {
-            //         console.log('Token no longer valid. Please log back in')
-            //         window.location.replace('/login')
-            //     }
-            // }
+            const keyUpdateResult = await fetchKeyUpdate(id, label, csrf)
+            if (!keyUpdateResult.error) {
+               this.updateKeys(id, label)
+            }
+
         }
+    }
+
+    updateKeys(id, label) {
+        let keyChanged = false
+        for (let i = 0; i < keyData.length; i++) {
+            if (keyData[i].id === id) {
+                keyData[i].label = label
+                keyChanged = true
+            }
+        }
+
+        if (keyChanged) {
+            this.state._rows = this.createRows(keyData)
+            this.setState({_rows: this.addBtnRows(this.state._rows)})
+        }
+
     }
 
     async setKeyFutureDisable () {
         console.log(`BTC Key Future Disable requested: ${id}`)
         const csrf = getCsrfToken()
         // const keyDeleteResult = await deleteKey('temp', csrf)
-    }
-
-    render() {
-        const {mini, tooltipPosition} = this.props;
-        const tooltipPos = tooltipPosition.split('-');
-        const tooltip = 'Create new Bitcoin Address'
-        const actions = [
-            <FlatButton
-                label="Save"
-                primary={true}
-                keyboardFocused={true}
-                onClick={this.saveKey.bind(this)}
-            />,
-            <FlatButton
-                label="Delete"
-                secondary={true}
-                keyboardFocused={true}
-                onClick={this.disableKey.bind(this)}
-            />,
-            <FlatButton
-                label="Okay"
-                primary={true}
-                keyboardFocused={true}
-                onClick={this.handleClose}
-            />
-        ];
-        return (
-            <div id="key-container">
-                <ReactDataGrid
-                    columns={this.state._columns}
-                    rowGetter={this.rowGetter}
-                    rowsCount={this.state._rows.length}
-                    rowHeight={48}
-                    minHeight={500}/>
-                <div style={keyBtnStyles} id="fab-container">
-                    <FloatingActionButton id="btc-key-btn"
-                                          mini={mini}
-                                          onClick={this.requestBtcKey}
-                                          onMouseEnter={() => this.setState({btnHovered: true})}
-                                          onMouseLeave={() => this.setState({btnHovered: false})}>
-
-                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                            <path fill="#000000"
-                                  d="M4.5,5H8V2H10V5H11.5V2H13.5V5C19,5 19,11 16,11.25C20,11 21,19 13.5,19V22H11.5V19H10V22H8V19H4.5L5,17H6A1,1 0 0,0 7,16V8A1,1 0 0,0 6,7H4.5V5M10,7V11C10,11 14.5,11.25 14.5,9C14.5,6.75 10,7 10,7M10,12.5V17C10,17 15.5,17 15.5,14.75C15.5,12.5 10,12.5 10,12.5Z"/>
-                        </svg>
-
-                    </FloatingActionButton>
-                    {tooltip &&
-                    <Tooltip
-                        show={this.state.btnHovered}
-                        label={tooltip}
-                        style={{fontSize: '9pt'}}
-                        horizontalPosition={tooltipPos[1]}
-                        verticalPosition={tooltipPos[0]}/>
-                    }
-                </div>
-            <Dialog
-                title="Key Editor"
-                actions={actions}
-                modal={false}
-                open={this.state.dialogOpen}
-                onRequestClose={this.handleClose}
-            >
-                <TextField
-                    value={this.state.dialogText}
-                    ref='keylabel'
-                    className='keylabel'
-                    floatingLabelText='Key Label'
-                    onChange={this.dialogTextChange}
-                />
-                <br />
-                Set a date to automatically disable this key <br/>
-                <div className="datepicker">
-                    <DatePicker hintText="Date Picker" />
-                    <FlatButton label="Set" secondary={true} keyboardFocused={true} onClick={this.setKeyFutureDisable}/>
-                </div>
-            </Dialog>
-
-    </div>
-        )
     }
 }
 
@@ -496,7 +508,7 @@ function fetchKey(url, csrf) {
 }
 
 function fetchKeyUpdate(id, label, csrf) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         const keyId = String('0000' + id).slice(-4)
         let url = `/api/key/${keyId}/update`
         let options = {
@@ -514,9 +526,12 @@ function fetchKeyUpdate(id, label, csrf) {
                     console.error('Error:', error)
                 ))
             .then(response => {
-                console.log('Success')
-                console.dir(response)
-                return resolve(response)
+                const handledResponse = handleResponse(response)
+                if (handledResponse) {
+                    return resolve(handledResponse)
+                } else {
+                    reject(handledResponse)
+                }
             })
     })
 }
@@ -534,6 +549,37 @@ function getToken(name) {
     if (cookie && cookie.length > 0) {
         return cookie[0].trim().substr(5)
     }
+}
+
+function handleResponse(response) {
+    if (Array.isArray(response) && response.length === 1) {
+        response = response[0]
+    } else {
+        console.error(response)
+    }
+    if ('error' in response) {
+        switch (response.code) {
+            case 400:
+                console.log(response.error)
+                break
+            case 401:
+                window.location.href = '/login'
+                break
+            case 403:
+                console.log(response.error)
+                break
+            case 404:
+                console.log(response.error)
+                break
+            case 406:
+                console.log(response.error)
+                console.log('You must must set acceptable return type in header')
+                break
+        }
+        console.log(response)
+
+    }
+    return response
 }
 
 
