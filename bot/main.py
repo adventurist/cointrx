b0fd3aeeffd067b6f00524631d3fe1db2dfcaa91
logicp <emmanuel@openconcept.ca>
2018-04-04 19:05:27 -0400

    new api endpoints, logic for bot application

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,14 @@
-import tornado.ioloop
 from tornado.websocket import WebSocketHandler, WebSocketError, StreamClosedError
 from tornado.web import Application, RequestHandler
 from tornado.log import enable_pretty_logging
-
-from wsclient import Bot
-
+from utils.wsclient import Bot
+from pythonjsonlogger import jsonlogger
 from types import SimpleNamespace
+
+import tornado.ioloop
 import logging
 import os
+import json
 
 enable_pretty_logging()
 socket_handlers = []
@@ -35,11 +36,59 @@ class StartHandler(RequestHandler):
         application.logger.info('Bots received')
 
 
+class BotAllHandler(RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+    def get(self, *args, **kwargs):
+        if len(application.bots) > 0:
+            for bot in application.bots:
+                application.logger.debug(bot.identify())
+
+
+class BotLoginHandler(RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+    async def get(self, *args, **kwargs):
+        if len(application.bots) > 0:
+            for bot in application.bots:
+                login_attempt = await bot.login()
+                if login_attempt:
+                    print('Good stuff')
+
+
+class BotDumpHandler(RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+    def get(self, *args, **kwargs):
+        if len(application.bots) > 0:
+            for bot in application.bots:
+                bot.dump_self()
+
+
+class BotTrcPriceHandler(RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+    async def get(self, *args, **kwargs):
+        if len(application.bots) > 0:
+            for bot in application.bots:
+                result = await bot.retrieve_price_history()
+                if result:
+                    print(result)
+
+
 class BotApplication(Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
-            (r"/start", StartHandler)
+            (r"/start", StartHandler),
+            (r"/bots/all", BotAllHandler),
+            (r"/bots/dump", BotDumpHandler),
+            (r"/bots/login", BotLoginHandler),
+            (r"/bots/trc/prices", BotTrcPriceHandler)
         ]
         settings = {
             "debug": True,
@@ -59,11 +108,13 @@ class BotApplication(Application):
             self.bots.append(bots)
 
 
-def setup_logger(name, level):
+def setup_logger(name, level, json_logging=False):
+
     logger = logging.getLogger(name)
     logger.setLevel(level)
     log_handler = logging.FileHandler('bot.log')
-    logger_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    logger_formatter = logging.Formatter(
+        '%(name)s - %(levelname)s - %(message)s') if json_logging is False else jsonlogger.JsonFormatter(json_indent=2)
     log_handler.setFormatter(logger_formatter)
 
     logger.addHandler(log_handler)
@@ -72,10 +123,18 @@ def setup_logger(name, level):
     return logger
 
 
+def user_data():
+    with open(os.path.join(os.path.dirname(__file__), 'users.json')) as d:
+        users = json.load(d)
+        if users is not None:
+            log.info('users retrieved: ' + str(users))
+    return users['users']
+
+
 def bot_config():
     config = SimpleNamespace()
-    config.url = 'ws://localhost:9977/'
-    config.timeout = 2000
+    config.timeout = 20000
+    config.number = -1
 
     return config
 
@@ -83,10 +142,12 @@ def bot_config():
 def make_bots():
     config = bot_config()
     bots = []
-
+    users = user_data()
     try:
-        for i in range(5):
-            new_bot = Bot(config, setup_logger('Bot ' + str(i), logging.DEBUG))
+        for i in range(len(users)):
+            config.number = i + 1
+            new_bot = Bot(config, setup_logger('Bot ' + str(i + 1), logging.DEBUG, json_logging=True))
+            new_bot.set_credentials(users[i])
             new_bot.connect()
             bots.append(new_bot)
 
@@ -107,5 +168,6 @@ def handle_error(err, name):
 
 if __name__ == "__main__":
     application = BotApplication()
+    log = application.logger
     application.listen(9977)
     tornado.ioloop.IOLoop.current().start()
