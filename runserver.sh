b6eea098dc8d7c66f4c64c9c0db5e8e58b74d781
logicp <emmanuel@openconcept.ca>
2018-04-21 01:27:06 -0400

    setting ENV variables properly

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /var/www/coinx/coinenv/bin/activate
+TRX_ENV=LOCAL_DEVELOPMENT
 python3.5 /var/www/coinx/main.py
--- runserver2.sh
+++ runserver2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /var/www/cointrx/coinenv/bin/activate
-TRX_ENV = LOCAL_DEVELOPMENT
+TRX_ENV=SNOWFLAKE
 python3.5 -u /var/www/cointrx/main.py
