3fbda8ea2be07369ec0c451e3e99c8d73b0b2b89
logicp <adventurist@gmail.com>
2019-04-07 10:51:15 -0400

    more options for TrxIcon SVGIcon abstraction

--- src/utils/component/TrxIcon.js
+++ src/utils/component/TrxIcon.js
@@ -3,10 +3,25 @@ import { Component } from 'react';
 
 export default class TrxIcon extends Component {
   render () {
+    let fontSize
+    switch (this.props.size) {
+      case undefined && 'sm':
+        fontSize = '24px'
+        break
+      case 'md':
+        fontSize = '32px'
+        break
+      case 'lg':
+        fontSize = '48px'
+        break
+      default:
+        fontSize = '24px'
+    }
+
     return (
-      <SvgIcon>
-        <svg style={{width:'24px', height:'24px'}} viewBox="0 0 24 24">
-          <path fill="#000000" d={this.props.path} />
+      <SvgIcon style={{fontSize: fontSize}}>
+        <svg style={{width: '24px', height: '24px'}} viewBox='0 0 24 24'>
+          <path fill={this.props.color || "#000000"} d={this.props.path} />
         </svg>
       </SvgIcon>
     )
