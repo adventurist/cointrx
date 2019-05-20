e7c19020602914ddb721cb76ec1e2aa8aad8b9b7
logicp <adventurist@gmail.com>
2019-05-20 14:56:45 -0400

    expanding bot functionality for bids and offers

--- bot/trade.py
+++ bot/trade.py
@@ -2,18 +2,20 @@ from utils.cointrx_client import Client
 import json
 import asyncio
 import os
+from types import SimpleNamespace
+
 http_client = Client()
 
 bots = []
+transaction_parts = SimpleNamespace(
+    offers=None,
+    bids=None
+)
 
 
 def user_data():
     with open(os.path.join(os.path.dirname(__file__), 'users.json')) as d:
         return json.load(d)['users']
-
-
-def login_bots():
-    pass
 
 
 async def login(credentials):
@@ -28,6 +30,7 @@ async def login(credentials):
 
     return body
 
+
 async def get_token():
     return await login(('banana', 'banana'))
 
@@ -41,10 +44,40 @@ async def start_bots(token):
         bots.append({'id': bot['id'], 'token': login_data['token'], 'refresh': login_data['refresh'], 'uid': login_data['uid'], 'name': login_data['name'], 'session': login_data['session_info']})
 
 
+async def get_offers():
+    response = await http_client.get('http://localhost:6969/offer')
+    body = json.loads(response.body.decode())
+    print('offers')
+    print(body)
+
+
+async def get_bids():
+    response = await http_client.get('http://localhost:6969/bid')
+    body = json.loads(response.body.decode())
+    print('bids')
+    print(body)
+
+
+async def get_transaction_parts():
+    transaction_parts.offers = await get_offers()
+    transaction_parts.bids = await get_bids()
+
+
+def create_matches():
+    for bot in bots:
+        matched_parts = [x for x in transaction_parts.offers if x.uid == bot.uid] + [x for x in transaction_parts.offers if x.uid == bot.uid]
+    if len(matched_parts) > 0:
+        pass
+    else:
+        # Do random action
+        pass
+
+
 async def main():
     login_data = await get_token()
     await start_bots(login_data['token'])
-    print(json.dumps(bots))
+    await get_transaction_parts()
+    create_matches()
 
 
 if __name__ == '__main__':
